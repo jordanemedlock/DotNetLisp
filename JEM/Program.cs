9cccc5a1e0d880fcf76b4c18237f4aa154d4394f
Jordan Medlock <jordan@catalysis.com>
2020-03-18 19:14:43 -0700

    Add project files.

---
+++ JEM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JEM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
